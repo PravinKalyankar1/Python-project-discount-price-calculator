53c8926f3cc341afb3e9bbfee617776d2f885f30
Pravinkalyankar97 <pravinkalyankar3@gmail.com>
2026-01-18 21:10:08 +0530

    First commit

---
+++ Project student marks.py
@@ -0,0 +1,5 @@
+Student= input("Enter the student name:")
+Marks_obtained= float(input("Marks Obtained:"))
+Total_marks=float(input("Total marks:"))
+percentage=(Marks_obtained/Total_marks)*100
+print("Result:", percentage,"%")
